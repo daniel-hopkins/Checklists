f481978ed2147ec519b93db497b30515b171c591
Daniel Hopkins <d@niestudio.com>
2014-04-27 16:28:09 -0700

    implementing cancel and done

---
+++ Checklists/AddItemViewController.h
@@ -0,0 +1,17 @@
+//
+//  AddItemViewController.h
+//  Checklists
+//
+//  Created by Daniel Hopkins on 4/27/14.
+//  Copyright (c) 2014 niestudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddItemViewController : UITableViewController
+
+- (IBAction)cancel;
+
+- (IBAction)done;
+
+@end
---
+++ Checklists/AddItemViewController.m
@@ -0,0 +1,54 @@
+//
+//  AddItemViewController.m
+//  Checklists
+//
+//  Created by Daniel Hopkins on 4/27/14.
+//  Copyright (c) 2014 niestudio. All rights reserved.
+//
+
+#import "AddItemViewController.h"
+
+@interface AddItemViewController ()
+
+@end
+
+@implementation AddItemViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)cancel
+{
+    [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+- (IBAction)done
+{
+    [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
